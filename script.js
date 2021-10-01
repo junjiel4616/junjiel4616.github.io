79ea7b1f8c5ac200ab9a556aff56140bf8841de8
junjiel4616 <91744988+junjiel4616@users.noreply.github.com>
2021-10-01 11:09:42 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let ownerName = "Your Name"
+let userName = "YourUsername";
+
+document.querySelectorAll(".owner-name").forEach((e) => {
+    e.innerHTML = ownerName;
+});
+
+document.querySelector("#github").href = "http://github.com/" + userName;
+document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
